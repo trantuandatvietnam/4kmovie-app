5547a8876ccbf722fd2fa7ff92ab8727f7008290
Trần Tuấn Đạt <76252195+trantuandatvietnam@users.noreply.github.com>
2021-12-03 20:21:40 +0700

    update

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: "https://api.themoviedb.org/3/",
+    apiKey: "92f1e6742a71a42560ce0af78ac6ddd4",
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/ogiginal/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+}
+
+export default apiConfig;
---
+++ src/api/axiosClient.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import queryString from "query-string";
+
+import apiConfig from "./apiConfig";
+
+const axiosClient = axios.create({
+    baseURL: apiConfig.baseUrl,
+    headers: {
+        "Content-Type": "application/json"
+    },
+    paramsSerializer: params => queryString.stringify({ ...params, api_key: apiConfig.apiKey }),
+})
+
+axios.interceptors.request.use(function (config) {
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+    if (response && response.data) {
+        return response;
+    }
+}, function (error) {
+    return Promise.reject(error);
+});
+
+export default axiosClient;
